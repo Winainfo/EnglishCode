efcf73e5770cf4c8bd26738f8c1bdb3c3a749dec
evans <368757898@qq.com>
2015-09-08 13:02:54 +0800

    增加第三方插件

---
+++ English/English/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  English
+//
+//  Created by Evans on 15/9/8.
+//  Copyright © 2015年 Evans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ English/English/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  English
+//
+//  Created by Evans on 15/9/8.
+//  Copyright © 2015年 Evans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
